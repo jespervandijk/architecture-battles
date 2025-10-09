94cc0e0063edf78e21392b2fcf8087fdabd07135
jespervandijk <j.vandijk@tjip.com>
2025-10-09 15:57:56 +0200

    progress

--- ModularMonolith_CleanArchitecture_VerticalSlices/StudentEnrollment/StudentEnrollment.Application/UseCases/Courses/CreateCourse.cs
+++ ModularMonolith_CleanArchitecture_VerticalSlices/StudentEnrollment/StudentEnrollment.Application/UseCases/Courses/CreateCourse.cs
@@ -1,8 +1,40 @@
-using System;
+using FastEndpoints;
+using StudentEnrollment.Domain.Courses;
 
 namespace StudentEnrollment.Application.UseCases.Courses;
 
-public class CreateCourse
+public class CreateCourseEndpoint : Endpoint<CreateCourse, CourseId>
+{
+    public override void Configure()
+    {
+        Post("api/course/create");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CreateCourse req, CancellationToken ct)
+    {
+
+        Response = await req.ExecuteAsync();
+    }
+}
+
+public class CreateCourse : ICommand<CourseId>
 {
 
+}
+
+public class CreateCourseValidator : Validator<CreateCourse>
+{
+    public CreateCourseValidator()
+    {
+
+    }
+}
+
+public class CreateCourseHandler : ICommandHandler<CreateCourse, CourseId>
+{
+    public Task<CourseId> ExecuteAsync(CreateCourse command, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
 }
