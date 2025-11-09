d56895b9f2ba2b6d63b43c17e99eda8aed243470
jespervandijk <j.vandijk@tjip.com>
2025-11-09 11:57:31 +0100

    progres

--- ModularMonolith_CleanArchitecture_VerticalSlices/AcademicManagement/AcademicManagement.Application/UseCases/Courses/GetCourses.cs
+++ ModularMonolith_CleanArchitecture_VerticalSlices/AcademicManagement/AcademicManagement.Application/UseCases/Courses/GetCourses.cs
@@ -1,10 +1,14 @@
 using AcademicManagement.Application.Abstractions.Repositories;
 using AcademicManagement.Domain.Aggregates.Courses;
+using AcademicManagement.Domain.Aggregates.Departments;
+using AcademicManagement.Domain.Aggregates.Professors;
+using AcademicManagement.Domain.Aggregates.Universities;
+using AcademicManagement.Domain.Scalars;
 using FastEndpoints;
 
 namespace AcademicManagement.Application.UseCases.Courses;
 
-public class GetAllCoursesEndpoint : EndpointWithoutRequest<IReadOnlyList<Course>>
+public class GetAllCoursesEndpoint : Endpoint<GetCourses, IReadOnlyList<Course>>
 {
     private readonly ICourseRepository _courseRepository;
 
@@ -19,13 +23,24 @@ public class GetAllCoursesEndpoint : EndpointWithoutRequest<IReadOnlyList<Course
         AllowAnonymous();
     }
 
-    public override async Task HandleAsync(CancellationToken ct)
+    public override async Task HandleAsync(GetCourses req, CancellationToken ct)
     {
         Response = await _courseRepository.GetAllAsync();
     }
 }
 
-public record GetCourses : ICommand<IReadOnlyList<Course>>
+public record GetCourses
 {
+    public UniversityId UniversityId { get; init; }
+    public DepartmentId DepartmentId { get; init; }
+    public ProfessorId CourseOwnerId { get; init; }
+    public ProfessorId ProfessorId { get; init; }
+    public Name Title { get; init; }
+}
 
+public class GetCoursesValidator : Validator<GetCourses>
+{
+    public GetCoursesValidator()
+    {
+    }
 }
