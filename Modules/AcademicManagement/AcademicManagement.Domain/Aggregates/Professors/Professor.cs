a667faae68f2fd086609faff192bd7c5b5113f3e
jespervandijk <j.vandijk@tjip.com>
2025-11-10 14:08:50 +0100

    professor domain update

--- Modules/AcademicManagement/AcademicManagement.Domain/Aggregates/Professors/Professor.cs
+++ Modules/AcademicManagement/AcademicManagement.Domain/Aggregates/Professors/Professor.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using AcademicManagement.Domain.Aggregates.Departments;
 using AcademicManagement.Domain.Aggregates.Universities;
 using AcademicManagement.Domain.Aggregates.Users;
@@ -5,46 +6,32 @@ using Qowaiv;
 
 namespace AcademicManagement.Domain.Aggregates.Professors;
 
-public class Professor
+public sealed class Professor
 {
-    public ProfessorId Id { get; private set; }
+    public ProfessorId Id { get; init; }
+    public UserId UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public EmailAddress EmailAddress { get; set; }
+    public UniversityId WorkPlace { get; set; }
+    public DepartmentId? DepartmentId { get; set; }
+    public Rank Rank { get; set; }
 
-    public UserId UserId { get; private set; }
-
-    public string FirstName { get; private set; }
-
-    public string LastName { get; private set; }
-
-    public EmailAddress EmailAddress { get; private set; }
-
-    public UniversityId WorkPlace { get; private set; }
-
-    public DepartmentId Department { get; private set; }
-
-    public Rank Rank { get; private set; }
-
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-    private Professor() { }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-
-    private Professor(string firstName, string lastName, EmailAddress emailAddress, Rank rank, UniversityId workPlace, UserId userId)
+    [JsonConstructor]
+    private Professor(ProfessorId id, string firstName, string lastName, EmailAddress emailAddress, Rank rank, UniversityId workPlace, UserId userId, DepartmentId? departmentId)
     {
-        Id = ProfessorId.Next();
+        Id = id;
         FirstName = firstName;
         LastName = lastName;
         EmailAddress = emailAddress;
         Rank = rank;
         WorkPlace = workPlace;
         UserId = userId;
+        DepartmentId = departmentId;
     }
 
-    public static Professor Create(string firstName, string lastName, EmailAddress emailAddress, Rank rank, UniversityId workPlace, UserId userId)
+    public static Professor Create(string firstName, string lastName, EmailAddress emailAddress, Rank rank, UniversityId workPlace, UserId userId, DepartmentId? departmentId = null)
     {
-        return new Professor(firstName, lastName, emailAddress, rank, workPlace, userId);
-    }
-
-    public void ChangeRank(Rank rank)
-    {
-        Rank = rank;
+        return new Professor(ProfessorId.Next(), firstName, lastName, emailAddress, rank, workPlace, userId, departmentId);
     }
 }
